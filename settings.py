9663ff470b60ebdb62dff4e8272ebdcf8d29c461
Василь Картичак <w.i.k.mailua@gmail.com>
2023-12-18 18:47:54 +0200

    installing

---
+++ settings.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Settings:
+    DATABASE_NAME = os.getenv('DATABASE_NAME', '')
+    DATABASE_USER = os.getenv('DATABASE_USER', '')
+    DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD', '')
+    DATABASE_HOST = os.getenv('DATABASE_HOST', '')
+    DATABASE_PORT = os.getenv('DATABASE_PORT', '')
+
+    @property
+    def DATABASE_URL(self) -> str:
+        return f'postgres+asyncpg://{self.DATABASE_USER}:{self.DATABASE_PASSWORD}@' \
+               f'{self.DATABASE_HOST}:{self.DATABASE_PORT}/{self.DATABASE_NAME}'
